2fdd0bb5dbad3581289dfeb9c8efad3c6f831d55
Sebas rwbu <lovelyrwbu69@gmail.com>
2023-07-26 00:26:58 +0700

    mengerjakan problem konversimenit

--- problem2.js
+++ problem2.js
@@ -1,11 +1,23 @@
 // Problem
-// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
-// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter 
+// berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit 
+// berdasarkan menit tersebut. 
 // Contoh, jika menit adalah 63, maka function akan me-return "1:03".
 
 function konversiMenit(menit) {
-    // you can only write your code here!
-}
+  let detik = menit % 60; 
+  let Menit = menit / 60; 
+  let pembulatan = Math.floor(Menit);
+
+  if(detik <= 9){
+    console.log(`${pembulatan}:0${detik}`);
+  }else{
+    console.log(`${pembulatan}:${detik}`);
+  }
+    
+}  
+
   
   // TEST CASES
   console.log(konversiMenit(63)); // 1:03
@@ -20,10 +32,11 @@ console.log('')
 
 //   Problem
 //   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
-//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, 
+//   dan false jika tidak.
 
 function xo(str) {
-    // you can only write your code here!
+  
 }
   
   // TEST CASES
