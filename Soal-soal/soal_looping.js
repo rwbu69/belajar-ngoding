5afa0d60356372a3eb8ca9c1c1a98df071174972
Sebas rwbu <lovelyrwbu69@gmail.com>
2023-08-01 23:38:57 +0700

    prettier time

--- Soal-soal/soal_looping.js
+++ Soal-soal/soal_looping.js
@@ -5,21 +5,19 @@
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
 
-// let i = 0; 
-// console.log('looping maju'); 
+// let i = 0;
+// console.log('looping maju');
 // while(i <= 5){
 //     console.log(i);
-//     i++ 
+//     i++
 // }
 
-// let j = 5; 
+// let j = 5;
 // console.log('looping mundur');
 // while(j >= 0){
 //     console.log(j);
 //     j--
 // }
-
-
 
 // 2. Melakukan Looping Menggunakan For
 // Problem
@@ -48,7 +46,7 @@
 // Apabila angka counter adalah angka genap, tuliskan GENAP
 // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
 
-// let genap = 0; 
+// let genap = 0;
 // let ganjil = 0 ;
 
 // for(let i = 1; i <= 100; i++){
@@ -57,24 +55,23 @@
 //         genap++
 //     }else{
 //         console.log('ganjil');
-//         ganjil++ 
+//         ganjil++
 //     }
 // }
 // console.log ('jumlah angka ganjil ada :', ganjil);
 // console.log ('jumlah angka genap ada :', genap);
-
 
 //4. counter kelipatan
 // Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
 // Pada 3 perulangan baru ini periksa setiap angka counter:
 
 // Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
-// Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, 
+// Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5,
 // dan kelipatan 10 dengan pertambahan 9, tuliskan:
 // "3 kelipatan 3"dan seterusnya.
 
 // let kelipatanDua = 0;
-// let kelipatanLima = 0; 
+// let kelipatanLima = 0;
 // let kelipatanSembilan = 0;
 
 // for(let i = 2; i <= 100; i+=2){
@@ -82,7 +79,7 @@
 //         console.log(`${i} adalah kelipatan 2`);
 //         kelipatanDua++;
 //     }else{}
-   
+
 // }
 // console.log('jumlah total kelipatan 2 ada',kelipatanDua);
 
